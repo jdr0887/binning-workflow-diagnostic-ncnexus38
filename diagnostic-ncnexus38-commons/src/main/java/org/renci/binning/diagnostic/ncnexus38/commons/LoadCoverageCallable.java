52b8acf139f00175cb762bbacf92a71730722536
Jason Reilly <jdr0887@gmail.com>
2017-02-01 12:47:01 -0500

    removing study...merging is done across samples which could come from different studies

--- diagnostic-ncnexus38-commons/src/main/java/org/renci/binning/diagnostic/ncnexus38/commons/LoadCoverageCallable.java
+++ diagnostic-ncnexus38-commons/src/main/java/org/renci/binning/diagnostic/ncnexus38/commons/LoadCoverageCallable.java
@@ -49,7 +49,6 @@ public class LoadCoverageCallable extends AbstractLoadCoverageCallable {
         logger.debug("ENTERING getDepthFile(String, Integer)");
         Map<String, String> avuMap = new HashMap<String, String>();
         avuMap.put("ParticipantId", participant);
-        avuMap.put("MaPSeqStudyName", "NCNEXUS38");
         avuMap.put("MaPSeqWorkflowName", "NCNEXUS38VariantCalling");
         avuMap.put("MaPSeqJobName", "SAMToolsDepth");
         avuMap.put("MaPSeqMimeType", "TEXT_PLAIN");
