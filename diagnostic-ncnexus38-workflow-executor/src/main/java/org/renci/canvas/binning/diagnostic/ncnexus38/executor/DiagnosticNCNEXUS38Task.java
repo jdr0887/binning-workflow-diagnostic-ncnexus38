94d48b5ae04d16214f30158c0d65e03b4e8c1315
Jason Reilly <jdr0887@gmail.com>
2017-05-25 09:05:18 -0400

    fixing path

--- diagnostic-ncnexus38-workflow-executor/src/main/java/org/renci/canvas/binning/diagnostic/ncnexus38/executor/DiagnosticNCNEXUS38Task.java
+++ diagnostic-ncnexus38-workflow-executor/src/main/java/org/renci/canvas/binning/diagnostic/ncnexus38/executor/DiagnosticNCNEXUS38Task.java
@@ -41,7 +41,7 @@ public class DiagnosticNCNEXUS38Task implements Runnable {
             RuntimeService runtimeService = bundleContext.getService(runtimeServiceReference);
 
             repositoryService.createDeployment()
-                    .addClasspathResource("org/renci/binning/diagnostic/ncnexus38/executor/ncnexus38.bpmn20.xml").deploy();
+                    .addClasspathResource("org/renci/canvas/binning/diagnostic/ncnexus38/executor/ncnexus38.bpmn20.xml").deploy();
 
             Map<String, Object> variables = new HashMap<String, Object>();
             variables.put("binningJobId", binningJobId);
@@ -49,7 +49,7 @@ public class DiagnosticNCNEXUS38Task implements Runnable {
             ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("ncnexus38_diagnostic_binning", variables);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage(), e);
         }
 
     }
